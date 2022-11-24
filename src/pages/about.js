bdc5a77d2398c53c382720bf0ef914bc32e68771
AinunNashwa <106902414+AinunNashwa@users.noreply.github.com>
2022-11-24 15:00:17 +0800

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,3 +13,14 @@ const AboutPage = () => {
 
 // Step 3: Export your component
 export default AboutPage
+
+export const Head = () => <title>About Me</title>
+
+export const Head = () => (
+  <>
+    <title>About Me</title>
+    <meta name="description" content="Your description" />
+  </>
+)
+
+
