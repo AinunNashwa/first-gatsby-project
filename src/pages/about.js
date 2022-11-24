c38922e296b0fd52db181336ce040bc1d5a5be0a
AinunNashwa <106902414+AinunNashwa@users.noreply.github.com>
2022-11-24 15:02:22 +0800

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,5 @@
-// Step 1: Import React
 import * as React from 'react'
 
-// Step 2: Define your component
 const AboutPage = () => {
   return (
     <main>
@@ -11,16 +9,6 @@ const AboutPage = () => {
   )
 }
 
-// Step 3: Export your component
-export default AboutPage
-
 export const Head = () => <title>About Me</title>
 
-export const Head = () => (
-  <>
-    <title>About Me</title>
-    <meta name="description" content="Your description" />
-  </>
-)
-
-
+export default AboutPage
