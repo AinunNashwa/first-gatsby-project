e6db7c418c281695b3385819a349b1fc430e0346
AinunNashwa <106902414+AinunNashwa@users.noreply.github.com>
2022-11-24 15:11:59 +0800

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,6 @@
-// Step 1: Import React
 import * as React from 'react'
 import { Link } from 'gatsby'
 
-// Step 2: Define your component
 const IndexPage = () => {
   return (
     <main>
@@ -13,8 +11,6 @@ const IndexPage = () => {
   )
 }
 
-// You'll learn about this in the next task, just copy it for now
 export const Head = () => <title>Home Page</title>
 
-// Step 3: Export your component
 export default IndexPage
