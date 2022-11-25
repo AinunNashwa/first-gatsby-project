911f291f127564835851b57bc09d89bdcd3e21f6
AinunNashwa <106902414+AinunNashwa@users.noreply.github.com>
2022-11-25 09:49:51 +0800

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,6 +10,15 @@ import {
 } from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
   return (
     <div className={container}>
       <nav>
